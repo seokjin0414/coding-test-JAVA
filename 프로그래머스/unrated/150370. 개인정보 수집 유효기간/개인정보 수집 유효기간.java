026b4fce9998928f748961aca14845c64c44f30c
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-06-23 14:15:24 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.91 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+  class Solution {
+        public Object solution(String today, String[] terms, String[] privacies) {
+            List<Integer> answer = new ArrayList<>();
+            Map<String,Integer> map = new HashMap<>();
+            int t = getDay(today);
+
+            for (String s : terms) {
+                map.put(s.split(" ")[0], Integer.parseInt(s.split(" ")[1]) * 28);
+            }
+
+            for (int i=0; i<privacies.length; i++) {
+                String[] a = privacies[i].split(" ");
+                 if (t >= getDay(a[0]) + (map.get(a[1]))) {
+                     answer.add(i+1);
+                 }
+            }
+            return answer;
+        }
+
+        private int getDay(String date) {
+            String[] d = date.split("\\.");
+            return (Integer.parseInt(d[0]) * 12 * 28) + (Integer.parseInt(d[1]) * 28) + Integer.parseInt(d[2]);
+        }
+
+    }
