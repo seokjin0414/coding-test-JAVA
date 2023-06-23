d90412a273b990b5c8c05150de392b01f1437f9a
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-06-23 14:14:17 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.39 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] rank, boolean[] attendance) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+            for (int i=0; i<rank.length; i++) {
+                if(attendance[i]) {
+                    map.put(rank[i], i);
+                }
+            }
+
+            List<Integer> rankList = new ArrayList<>(map.keySet());
+            rankList.sort(Comparator.naturalOrder());
+
+            int answer = (10000 * map.get(rankList.get(0))) + (100 * map.get(rankList.get(1))) + map.get(rankList.get(2));
+            
+            return answer;
+    }
+}
