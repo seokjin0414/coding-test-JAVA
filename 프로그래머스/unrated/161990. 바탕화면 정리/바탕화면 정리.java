7014e860eb82d15d21bb4322adda36db9d9cbb00
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-06-23 14:13:34 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.12 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public Object solution(String[] wallpaper) throws Exception{
+            List<Integer> answer = new ArrayList<>();
+            int maxH = wallpaper.length;
+            int minH = 0;
+            int maxW = 0;
+            int minW = wallpaper[0].length();
+
+            for (int i=0; i< wallpaper.length; i++) {
+                if (wallpaper[i].contains("#")) {
+                    if (maxH >=i) {
+                        maxH = i;
+                    }
+                    minH = i;
+
+                    if (minW > wallpaper[i].indexOf("#")) {
+                        minW = wallpaper[i].indexOf("#");
+                    }
+
+                    if(maxW < wallpaper[i].lastIndexOf("#")) {
+                        maxW = wallpaper[i].lastIndexOf("#");
+                    }
+                }
+            }
+
+            answer.add(maxH);
+            answer.add(minW);
+            answer.add(minH+1);
+            answer.add(maxW+1);
+
+            return answer;
+        }
+}
