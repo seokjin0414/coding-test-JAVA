c17e54a837fb48e34122a2177e142983f2ba255d
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-06-28 23:56:55 +0900

    [level 1] Title: 예산, Time: 0.34 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+
+        Arrays.sort(d);
+        
+        for (int m : d) {
+            if (m <= budget) {
+                answer++;
+                budget-=m;
+            } else {
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
