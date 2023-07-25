e018f86976bd22bfbc4556a72702980d6d990633
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-07-25 16:47:29 +0900

    [level 2] Title: 땅따먹기, Time: 18.72 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,18 @@
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][2]));
+        }
+
+        for (int j = 0; j < 4; j++) {
+            answer = Math.max(answer, land[land.length- 1 ][j]);
+        }
+
+        return answer;
+    }
+}
