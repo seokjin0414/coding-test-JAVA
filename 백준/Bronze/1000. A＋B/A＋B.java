fbbd31ca9dff112149af5369ed08ebeb855b359b
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-08-13 20:17:25 +0900

    [Bronze V] Title: A+B, Time: 108 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+	public static void main(String args[]){
+		Scanner kb = new Scanner(System.in);
+		int a, b;
+		a = kb.nextInt();
+		b = kb.nextInt();
+		System.out.println(a + b);
+	}
+}
