e50244fa314696156ae751a8d26e43451471970b
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-09-15 20:00:28 +0900

    [unrated] Title: 할 일 목록, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public List<String> solution(String[] todo_list, boolean[] finished) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < todo_list.length; i++) {
+            if (!finished[i]) {
+                list.add(todo_list[i]);
+            }
+        }
+        return list;
+    }
+}
