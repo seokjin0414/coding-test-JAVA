2bc06641c511e8767ef53c75a830a115e4ca4c82
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-10-08 18:14:44 +0900

    [unrated] Title: 부분 문자열, Time: 0.03 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String str1, String str2) {
+        if (str2.indexOf(str1) != -1) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
