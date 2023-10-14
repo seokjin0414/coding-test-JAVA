ca5f94ad52cef9ce6b1100656295deb9844f3d9d
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-10-14 23:50:28 +0900

    [level unrated] Title: 합성수 찾기, Time: 0.05 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        if (n > 2) {
+            answer = n - 2;
+            for (int i=3; i<=n; i++) {
+                if (i % 2 == 0) {
+                    continue;
+                }
+
+                int cnt = 0;
+
+                for (int j=3; j<=Math.sqrt(i); j+=2) {
+                    if (i != 3 && i % j == 0) {
+                        cnt ++;
+                        break;
+                    }
+                }
+
+                if (cnt == 0) {
+                    answer--;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
