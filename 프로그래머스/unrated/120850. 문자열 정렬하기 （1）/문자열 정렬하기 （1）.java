ef42e52ccffc678c73e0e9552823287673420a95
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-10-17 12:22:58 +0900

    [level unrated] Title: 문자열 정렬하기 (1), Time: 0.27 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+class Solution {
+    public List<Integer> solution(String my_string) {
+        List<Integer> list = new ArrayList<>();
+
+        for (char c : my_string.toCharArray()) {
+            if (90 > c) {
+                list.add(c - 48);
+            }
+        }
+
+        Collections.sort(list);
+
+        return list;
+    }
+}
