ef8d348806e19d21b2947844383dafbefbd635d4
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-10-20 23:40:50 +0900

    [level unrated] Title: 소인수분해, Time: 0.34 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/unrated/120852. 소인수분해/소인수분해.java
@@ -1,37 +1,19 @@
 import java.util.*;
-import java.util.stream.Collectors;
 class Solution {
-     public List<Integer> solution(int n) {
+    public List<Integer> solution(int n) {
         List<Integer> list = new ArrayList<>();
 
-        while (n > 1) {
-            if (n % 2 == 0) {
-                list.add(2);
-                n /= 2;
-            } else {
-                for (int i = 3; i <= n; i++) {
-                    if (i % 2 == 0) {
-                        continue;
-                    }
-
-                    int cnt = 0;
-
-                    for (int j = 3; j <= Math.sqrt(i); j += 2) {
-                        if (i != 3 && i % j == 0) {
-                            cnt++;
-                            break;
-                        }
-                    }
-
-                    if (cnt == 0 && n % i == 0) {
-                        list.add(i);
-                        n /= i;
-                        break;
-                    }
+        for (int i = 2; i <= n; i++) {
+            if (n % i ==0) {
+                list.add(i);
+                while (n % i == 0) {
+                    n /= i;               
                 }
             }
         }
 
-        return list.stream().distinct().sorted().collect(Collectors.toList());
+        list.sort(Comparator.naturalOrder());
+        
+        return list;
     }
 }
