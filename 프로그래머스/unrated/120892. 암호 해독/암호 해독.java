473b9ed7b348dfc5c80cc2b70eb60de745573807
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-10-24 22:12:47 +0900

    [level unrated] Title: 암호 해독, Time: 0.04 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        StringBuilder sb  = new StringBuilder();
+        
+        for (int i = code; i <= cipher.length(); i+=code) {
+            sb.append(cipher.charAt(i - 1));
+        }
+        
+        return sb.toString();
+    }
+}
