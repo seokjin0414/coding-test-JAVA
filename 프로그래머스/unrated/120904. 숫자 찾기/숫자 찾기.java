72df2b98dbb8e8456dd98565284ca6444da3951b
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2023-10-30 23:44:11 +0900

    [level unrated] Title: 숫자 찾기, Time: 0.03 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = -1;
+        String s = String.valueOf(num);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) -48 == k) {
+                answer = i + 1;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
