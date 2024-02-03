d665288484e8724f743464bac3bd2b6a1daf7fd5
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2024-02-03 22:45:20 +0900

    [Bronze IV] Title: 영수증, Time: 120 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,27 +1,28 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer st = new StreamTokenizer(System.in);
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        int line = Integer.parseInt(br.readLine());
-        int n = Integer.parseInt(br.readLine());
+
+        st.nextToken();
+        int total = (int)st.nval;
+        st.nextToken();
+        int n = (int)st.nval;
         int sum = 0;
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            sum += Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
+        for(int i = 0; i < n; i++){
+            st.nextToken();
+            int price = (int)st.nval;
+            st.nextToken();
+            int cnt= (int)st.nval;
+            sum += (price * cnt);
         }
-
-        if (line == sum) {
-            bw.write("Yes");
-        } else {
+        if(total == sum) {
+            bw.write("Yes");}
+        else {
             bw.write("No");
         }
-
-        br.close();
         bw.flush();
         bw.close();
     }
