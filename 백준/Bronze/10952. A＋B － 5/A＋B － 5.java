d4f0710b50dfe2e37687d4825678debc4ffa85dc
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2024-02-11 21:07:18 +0900

    [Bronze V] Title: A+B - 5, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -2,19 +2,20 @@ import java.io.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
+        int a, b;
         StreamTokenizer st = new StreamTokenizer(System.in);
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         while (true) {
             st.nextToken();
-            int a = (int) st.nval;
+            a = (int) st.nval;
 
             if(a == 0) {
                 break;
             }
 
             st.nextToken();
-            int b = (int) st.nval;
+            b = (int) st.nval;
 
             bw.write((a +  b) + "\n");
         }
