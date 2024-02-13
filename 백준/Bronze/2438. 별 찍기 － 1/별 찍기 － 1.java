6a84eb9fcc5fcbbcab35346472f664cabed568dd
seokjin shin <80299204+seokjin0414@users.noreply.github.com>
2024-02-13 21:11:02 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 76 ms, Memory: 11580 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -3,20 +3,16 @@ import java.io.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int cnt;
         int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        
-        for (int i = 1; i <= n; i++) {
-            cnt = i;
-            while (cnt > 0) {
-                sb.append("*");
-                cnt--;
-            }
-            sb.append("\n");
+        StringBuilder sbLine = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            sb.append("*");
+            sbLine.append(sb + "\n");
         }
-        
-        System.out.println(sb.toString());
+
+        System.out.println(sbLine);
         br.close();
     }
 }
