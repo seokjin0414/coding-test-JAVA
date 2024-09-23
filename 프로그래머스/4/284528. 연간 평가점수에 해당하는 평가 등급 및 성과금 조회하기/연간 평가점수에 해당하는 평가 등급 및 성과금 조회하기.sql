7af2e3a1507b7d8795806e23fedd6f48853d06b5
seokjin shin <sars21@hanmail.net>
2024-09-23 19:32:00 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,17 @@
+WITH checked AS (
+    SELECT EMP_NO, 
+        CASE WHEN AVG(SCORE) >= 96 THEN 'S'
+        WHEN AVG(SCORE) >= 90 THEN 'A'
+        WHEN AVG(SCORE) >= 80 THEN 'B'
+        ELSE 'C' END GRADE, 
+        CASE WHEN AVG(SCORE) >= 96 THEN 0.2
+        WHEN AVG(SCORE) >= 90 THEN 0.15
+        WHEN AVG(SCORE) >= 80 THEN 0.10
+        ELSE 0 END per
+    FROM HR_GRADE
+    GROUP BY 1
+)
+SELECT e.EMP_NO, e.EMP_NAME, c.GRADE, e.SAL * c.per BONUS
+FROM HR_EMPLOYEES e
+INNER JOIN checked c USING (EMP_NO)
+ORDER BY 1
